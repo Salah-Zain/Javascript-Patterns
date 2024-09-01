8348c6baec45dcc04a3e25ce91b15e9ca740402e
Salah-Zain <salahzain36@gmail.com>
2024-09-01 13:27:54 +0530

    Hour-glass Pattern

---
+++ xv) Hour-glass Pattern/index.js
@@ -0,0 +1,29 @@
+function pattern(num){
+    for(i=1; i<=num; i++){
+        str = '';
+        for(j=num; j>=i; j--){
+            str += ' *';
+        }
+        let space = '';
+        for(k=1; k<=i; k++){
+            space += ' '
+        }
+        console.log(space + str);
+    }
+
+    for(i=1; i<=num; i++){
+        let str = '';
+        for(j=1; j<=i; j++){
+            str += ' *';
+        }
+        let space = '';
+        for(k=num; k>=i; k--){
+            space += ' '
+        }
+        console.log(space + str);
+        
+    }
+    console.log(pattern);
+    
+}
+pattern(5);
